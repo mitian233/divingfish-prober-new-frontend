62a1476bbff33bf11fd14cf1a7633c2686f8bf07
mitian233 <mitian233@yahoo.co.jp>
2024-05-16 00:22:02 +0800

    fix reference

--- src/plugins/watchVisible.ts
+++ src/plugins/watchVisible.ts
@@ -1,31 +1,31 @@
-let manualPopstate: boolean = false;
-let listenerList: Array<any> = [];
-function popstateListener() {
-    let listener = listenerList.shift();
-    if (!manualPopstate) {
-        listener.that[listener.property] = false;
-    }
-    if (!listenerList.length)
-        window.removeEventListener("popstate", popstateListener);
-}
-export default function watchVisible(property:any, title:any, that:any) {
-    return function (visible:any) {
-        if (!that) that = this;
-        if (visible) {
-            window.history.pushState(
-                { title: title, url: `#${title}` },
-                title,
-                `#${title}`
-            );
-            if (!listenerList.length)
-                window.addEventListener("popstate", popstateListener);
-            listenerList.unshift({that: that, property: property});
-        } else {
-            if (window.history.state && window.history.state.title === title) {
-                manualPopstate = true;
-                window.history.back();
-                manualPopstate = false;
-            }
-        }
-    };
-}
+// let manualPopstate: boolean = false;
+// let listenerList: Array<any> = [];
+// function popstateListener() {
+//     let listener = listenerList.shift();
+//     if (!manualPopstate) {
+//         listener.that[listener.property] = false;
+//     }
+//     if (!listenerList.length)
+//         window.removeEventListener("popstate", popstateListener);
+// }
+// export default function watchVisible(property:any, title:any, that:any) {
+//     return function (visible:any) {
+//         if (!that) that = this;
+//         if (visible) {
+//             window.history.pushState(
+//                 { title: title, url: `#${title}` },
+//                 title,
+//                 `#${title}`
+//             );
+//             if (!listenerList.length)
+//                 window.addEventListener("popstate", popstateListener);
+//             listenerList.unshift({that: that, property: property});
+//         } else {
+//             if (window.history.state && window.history.state.title === title) {
+//                 manualPopstate = true;
+//                 window.history.back();
+//                 manualPopstate = false;
+//             }
+//         }
+//     };
+// }
